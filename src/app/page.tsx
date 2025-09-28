d1ccc0a8914d962c23a81f9a8c3688e315ec4743
jorgeluis10 <jorgerodriguez1053@gmail.com>
2025-09-28 11:03:28 -0300

    First Version

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Digital Money House</h1>
+      <p className="mt-4">Tu billetera virtual</p>
+    </main>
+  );
+}
