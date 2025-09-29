6117aeb56db8e9036992b83b4b5b970e97b7e4ed
jorgeluisra2004 <jorgeluisra2004@gmail.com>
2025-09-29 00:38:19 -0300

    Login y Registro Completos 3

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,17 @@
+// lib/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("❌ Faltan variables de entorno de Supabase.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
